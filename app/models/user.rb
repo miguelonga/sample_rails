13276f41ea3b600caedf43171288abf2e84dacb8
Miguel Mundi <miguelmundiaragones@gmail.com>
2016-03-02 22:31:31 +0100

    after users with secure pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates  :name, presence: true,
                     length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
                     length: { maximum: 255 }, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end

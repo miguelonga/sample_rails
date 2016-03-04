a062f338420cf87abc1891806926b607bf2a2af3
Miguel Mundi <miguelmundiaragones@gmail.com>
2016-03-04 18:29:59 +0100

    CRUD users

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails', '4.0.3'
 gem 'turbolinks',   '2.3.0'
 gem 'bcrypt',       '3.1.7'
 gem 'jbuilder',     '2.2.3'
+gem 'faker',                '1.4.2'
 gem 'sdoc',         '0.4.0', group: :doc
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302212011) do
+ActiveRecord::Schema.define(version: 20160304162845) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160302212011) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end

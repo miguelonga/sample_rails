4e8f0bf50386d6d96245f7d666990939837466e6
Miguel Mundi <miguelmundiaragones@gmail.com>
2016-03-05 10:34:01 +0100

    refactored _error_messages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if logged_in?
+    @feed_items = current_user.microposts
   end
 
   def help

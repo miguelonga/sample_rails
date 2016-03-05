ff6b86d9e26bd4cacbf81e2e2418c460a214d8db
Miguel Mundi <miguelmundiaragones@gmail.com>
2016-03-05 09:27:07 +0100

    account confirmation and password reset finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all
+    @users = User.where(activated: true)
   end
 
   def new
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
